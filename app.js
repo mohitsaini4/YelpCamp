8a3ad6fac60b71a6dc15bc5cc581b0b01005eb2d
Mohit <mohitaprilsaini@gmail.com>
2019-07-30 13:31:06 -0400

    navbar added to the header.ejs

--- app.js
+++ app.js
@@ -6,15 +6,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 
 var campgrounds = [
-	{name: "creek", image: "https://pixabay.com/get/5fe8d1434852b108f5d084609620367d1c3ed9e04e50744f712d7ad09044c2_340.jpg"}, 
-	{name: "hill", image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732b79d5934acc5f_340.jpg"}, 
-	{name: "creek", image: "https://pixabay.com/get/5fe8d1434852b108f5d084609620367d1c3ed9e04e50744f712d7ad09044c2_340.jpg"}, 
-	{name: "hill", image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732b79d5934acc5f_340.jpg"}, 
-	{name: "creek", image: "https://pixabay.com/get/5fe8d1434852b108f5d084609620367d1c3ed9e04e50744f712d7ad09044c2_340.jpg"}, 
-	{name: "hill", image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732b79d5934acc5f_340.jpg"}, 
-	{name: "creek", image: "https://pixabay.com/get/5fe8d1434852b108f5d084609620367d1c3ed9e04e50744f712d7ad09044c2_340.jpg"}, 
-	{name: "hill", image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732b79d5934acc5f_340.jpg"}, 
-	{name: "mountain", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c732b79d5934acc5f_340.jpg"}
+	{name: "creek", image: "https://pixabay.com/get/52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c732b78dd9048c558_340.jpg"}, 
+	{name: "creek", image: "https://pixabay.com/get/52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c732b78dd9048c558_340.jpg"}
+	
 	]
 
 app.get("/", function(req, res){
