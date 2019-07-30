c70c3843aa3cddcc860947995e0796b67b037392
Mohit <mohitaprilsaini@gmail.com>
2019-07-30 16:25:11 -0400

    data connected with mongoose

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+Campground.create(
+	{
+		name:"Salmon",
+		image:"https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c732b7ed5904bc05e_340.jpg"
+	}
+	, function(err, campground){
+	if(err){
+		console.log(err);
+	}
+	else{
+		console.log("new campground created");
+	}
+
+	});
 
 var campgrounds = [
 	{name: "creek", image: "https://pixabay.com/get/52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c732b78dd9048c558_340.jpg"}, 
