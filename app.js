ccaf2b0335aa6d15b5d03953d69366cb0a55bfae
Mohit <mohitaprilsaini@gmail.com>
2019-08-20 12:54:46 -0400

    packages installed for user authentication

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 var seedDB = require("./seeds.js");
 var Comment = require("./models/comment");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var User = require("./models/user");
 
 
 mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
@@ -13,8 +16,21 @@ app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 
 seedDB();
-//SCHEMA SETUP
+app.use(require("express-session")({
+	secret: "My name is mohit Saini",
+	resave:false,
+	saveUninitialized:false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+//SCHEMA SETUP
 app.get("/", function(req, res){
 	res.render("landing.ejs");
 });
@@ -103,6 +119,12 @@ app.post("/campgrounds/:id/comments", function(req, res){
 			});
 		}
 	});
+});
+
+
+////++++++++++
+app.get("/register", function(req, res){
+	res.render("register.ejs");
 });
 
 
