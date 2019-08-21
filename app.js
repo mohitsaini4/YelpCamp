d3583bf89b4f35f9e0c97b2860216bb2febcb3fd
Mohit <mohitaprilsaini@gmail.com>
2019-08-20 21:49:08 -0400

    refactoring of the code

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 
-seedDB();
+//seedDB();
 app.use(require("express-session")({
 	secret: "My name is mohit Saini",
 	resave:false,
